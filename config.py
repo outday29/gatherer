cfc78b30e0d1af98e11f9c118149d7ec0d1be36e
Ong Eng Kheng <ongengkheng929@gmail.com>
2022-04-14 13:26:28 +0800

    fix:add config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+TIMES_LOGIN_URL = "https://times.taylors.edu.my/login/index.php"
+TIMES_HOMEPAGE_URL = "https://times.taylors.edu.my/my/"
+
+DATA_PATH = Path("D:/gatherer/" )
+PERSONAL_PATH = DATA_PATH / "personal.json"
+DEADLINES_PATH = DATA_PATH / "deadlines.csv"
+LOG_PATH = DATA_PATH / "log.txt"
+
+COURSE_MATERIAL_PATH = Path("D:/School_Material/")
